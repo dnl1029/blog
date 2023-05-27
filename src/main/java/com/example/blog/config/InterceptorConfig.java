a7bed644ca001187ad46ed4b9ac895a57642b05d
kyu <dnl1029@gmail.com>
2023-05-27 19:09:30 +0900

    23.05.27 인터셉터 config 수정

--- src/main/java/com/example/blog/config/InterceptorConfig.java
+++ src/main/java/com/example/blog/config/InterceptorConfig.java
@@ -1,19 +1,22 @@
 package com.example.blog.config;
 
 import com.example.blog.interceptor.MyInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor())
                 .order(1)
-                .addPathPatterns("/*")
-                .excludePathPatterns("/css/**","/*.ico","/error");
+                //.addPathPatterns("/api/*")
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
     }
 
 }

83ed368e71f27331fbe9b2a9ab01b6542d64f721
dnl1029 <dnl1029@gmail.com>
2023-07-21 00:22:13 +0900

    yml파일 가져오는 방법 비교

--- src/main/java/com/example/blog/service/MyServiceImpl.java
+++ src/main/java/com/example/blog/service/MyServiceImpl.java
@@ -1,13 +1,31 @@
 package com.example.blog.service;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@ConfigurationProperties(prefix = "custom.value")
+@Getter
+@Setter
 public class MyServiceImpl implements MyService{
+
+    private String x;
+    private String y;
+
+    @Value("${custom.value_v2.z}")
+    private String z;
+
     @Override
     public void mylog() {
         log.info("mylog test...");
+        log.info("x : {}",x);
+        log.info("y : {}",y);
+        log.info("z : {}",z);
+
     }
 }

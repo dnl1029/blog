ad63e216b3d8c42738723ad6c58844d87b538f18
dnl1029 <dnl1029@gmail.com>
2023-07-31 22:32:41 +0900

    Async 코드 추가

--- src/main/java/com/example/blog/event/EventSubService.java
+++ src/main/java/com/example/blog/event/EventSubService.java
@@ -2,7 +2,10 @@ package com.example.blog.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +13,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EventSubService {
 
+    @Qualifier("eventListenerThreadExecutor")
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    @Async("eventListenerThreadExecutor")
     @EventListener
     public void subscribe(EventDto eventDto) {
-        log.info("EvnetSubService. event: {}", eventDto);
+        log.info("EventSubService. event: {}", eventDto);
+        log.info("thread / corePool : {}, maxPool : {}, QueueCapacity : {}, ActiveCount : {}",
+                threadPoolTaskExecutor.getCorePoolSize(),
+                threadPoolTaskExecutor.getMaxPoolSize(),
+                threadPoolTaskExecutor.getQueueCapacity(),
+                threadPoolTaskExecutor.getActiveCount());
+
     }
 
 }
